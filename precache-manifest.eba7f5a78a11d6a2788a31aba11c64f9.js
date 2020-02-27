f0deee83ff852615374c8f47649f9e603db38618
anton_kuchkov <kuanton@mail.ru>
2020-02-28 00:18:53 +0300

    Updates

---
+++ precache-manifest.eba7f5a78a11d6a2788a31aba11c64f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45bfaca57156a18f58c747b9c9bb6f42",
+    "url": "/ks-userPosts/index.html"
+  },
+  {
+    "revision": "ae3807547f7abe84cbff",
+    "url": "/ks-userPosts/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "69468445acc4d7b07433",
+    "url": "/ks-userPosts/static/css/main.4d82f52d.chunk.css"
+  },
+  {
+    "revision": "ae3807547f7abe84cbff",
+    "url": "/ks-userPosts/static/js/2.c7025431.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/ks-userPosts/static/js/2.c7025431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69468445acc4d7b07433",
+    "url": "/ks-userPosts/static/js/main.cdd8ad29.chunk.js"
+  },
+  {
+    "revision": "76484f30e869a116bf91",
+    "url": "/ks-userPosts/static/js/runtime-main.1fa6b889.js"
+  }
+]);
